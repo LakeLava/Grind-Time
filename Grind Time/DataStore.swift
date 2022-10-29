84aa1ae0b836e3e78e8b2e1148cee59ea1ab477c
Max BeGole <max@begole.com>
2022-10-29 16:03:41 -0400

    I made 8 example elements in both of the arrays at the bottom of Database file

--- Grind Time/DataStore.swift
+++ Grind Time/DataStore.swift
@@ -132,19 +132,41 @@ extension DataStore {
     }
      */
 }
+let dateFormatter = DateFormatter()
+let date1 = Date(timeIntervalSinceReferenceDate: 410220000)
 
 extension DataStore {
     static let sampleData: DataStore = DataStore(
         locationHistories: [
+            LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 40.567896, longitude: 80.456789), date: date1),
+            LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 12.344556, longitude: -12.234556),  date: date1+100000),
+            LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 45.676765, longitude: -20.444490), date:  date1+200000),
+            LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 11.111111, longitude: -11.111111), date: date1+300000),
+            LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 13.333333, longitude: -13.333333), date: date1+400000),
+            LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 30.384920, longitude: -79.4567890), date: date1+500000),
             LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 34.676975, longitude: -82.836354), date: Date.now),
-            LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 34.676965, longitude: -82.836364), date: Date.now)
+            LocationHistory(coordinate: CLLocationCoordinate2D(latitude: 34.676965, longitude: -82.836364), date: Date.now),
         ],
         timeAtBuildings: [
             TimeAtBuilding(building: Location.cooperLibrary, totalTime: TimeInterval(49231),
-                           lastSessionDuration: TimeInterval(12301), lastSessionDate: Date.now),
-            TimeAtBuilding(building: Location.hendrixStudentCenter, totalTime: TimeInterval(29231),
-                           lastSessionDuration: TimeInterval(11301), lastSessionDate: Date.now)
+                           lastSessionDuration: TimeInterval(12301), lastSessionDate: date1+600000),
+            TimeAtBuilding(building: Location.hendrixStudentCenter, totalTime: TimeInterval(12245),
+                           lastSessionDuration: TimeInterval(11301), lastSessionDate: date1+800000),
+            TimeAtBuilding(building: Location.alumniCenter, totalTime: TimeInterval(67890),
+                           lastSessionDuration: TimeInterval(12314), lastSessionDate: date1+900000),
+            TimeAtBuilding(building: Location.Classof1944, totalTime: TimeInterval(13579),
+                           lastSessionDuration: TimeInterval(54657), lastSessionDate: date1+1000000),
+            TimeAtBuilding(building: Location.administrativeServicesBuilding, totalTime: TimeInterval(24680),
+                           lastSessionDuration: TimeInterval(23455), lastSessionDate: date1+1100000),
+            TimeAtBuilding(building: Location.barnettHall, totalTime: TimeInterval(63740),
+                           lastSessionDuration: TimeInterval(24790), lastSessionDate: date1+1200000),
+            TimeAtBuilding(building: Location.biologicalSciencesFieldStation, totalTime: TimeInterval(12906),
+                           lastSessionDuration: TimeInterval(12380), lastSessionDate: date1+1300000),
+            TimeAtBuilding(building: Location.brooksCenterforthePerformingArts, totalTime: TimeInterval(23568),
+                           lastSessionDuration: TimeInterval(12377), lastSessionDate: date1+1400000),
+            
 
         ]
-    )
+    
+            )
 }
