f5fa433155a458759fe3052f106a6901fc81deae
Max BeGole <max@begole.com>
2022-10-29 08:53:15 -0400

    Initial Commit

---
+++ Grind Time/Grind_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grind_TimeApp.swift
+//  Grind Time
+//
+//  Created by Max BeGole on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Grind_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
